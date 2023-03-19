50bdc6be4c585efa69b00a8a774d9c271439d1de
Mikhail Tvarkovsky <mtvarkovsky@gmail.com>
2023-03-20 02:16:58 +0400

    Add job for simple delete user example

---
+++ examples/deleteuser/simple/job.gen.go
@@ -0,0 +1,150 @@
+// This code is automatically generated. EDIT AT YOUR OWN RISK.
+
+package simple
+
+import (
+	"fmt"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser/dummyservices"
+	"github.com/mtvarkovsky/goodjob/pkg/interfaces"
+	"github.com/oklog/ulid/v2"
+)
+
+type SimpleDeleteUserDataJob struct {
+	LastTask       interfaces.Task
+	LastTaskPos    int
+	LastTaskResult *interfaces.TaskResult
+	ID             interfaces.JobID
+	JobArgs        []interfaces.JobArg
+	Tasks          []interfaces.Task
+	TaskArgs       map[interfaces.TaskID][]*interfaces.TaskArg
+	Visible        bool
+}
+
+func NewSimpleDeleteUserDataJob(userID string, usersClient *dummyservices.UserServiceClient, authClient *dummyservices.AuthServiceClient, ordersClient *dummyservices.OrdersServiceClient) interfaces.Job {
+	jobID := interfaces.JobID(fmt.Sprintf("delete user data (%s)", ulid.Make()))
+	jobArgs := []interfaces.JobArg{
+		{
+			Name:  "userID",
+			Value: userID,
+		},
+		{
+			Name:  "usersClient",
+			Value: usersClient,
+		},
+		{
+			Name:  "authClient",
+			Value: authClient,
+		},
+		{
+			Name:  "ordersClient",
+			Value: ordersClient,
+		},
+	}
+
+	taskIDs := map[string]interfaces.TaskID{
+		"safe delete user auth data":   interfaces.TaskID(fmt.Sprintf("safe delete user auth data (%s)", ulid.Make())),
+		"safe delete user data":        interfaces.TaskID(fmt.Sprintf("safe delete user data (%s)", ulid.Make())),
+		"safe delete user orders data": interfaces.TaskID(fmt.Sprintf("safe delete user orders data (%s)", ulid.Make())),
+	}
+	tasks := []interfaces.Task{
+		deleteuser.SafeDeleteAuthDataTask{
+			ID:    taskIDs["safe delete user auth data"],
+			JobID: jobID,
+		},
+		deleteuser.SafeDeleteUserTask{
+			ID:    taskIDs["safe delete user data"],
+			JobID: jobID,
+		},
+		deleteuser.SafeDeleteUserOrdersTask{
+			ID:    taskIDs["safe delete user orders data"],
+			JobID: jobID,
+		},
+	}
+	taskArgs := map[interfaces.TaskID][]*interfaces.TaskArg{
+		taskIDs["safe delete user auth data"]: {
+			{
+				Name:  "authClient",
+				Value: authClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+		taskIDs["safe delete user data"]: {
+			{
+				Name:  "usersClient",
+				Value: usersClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+		taskIDs["safe delete user orders data"]: {
+			{
+				Name:  "ordersClient",
+				Value: ordersClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+	}
+	return &SimpleDeleteUserDataJob{
+		JobArgs:        jobArgs,
+		LastTaskPos:    0,
+		Tasks:          tasks,
+		TaskArgs:       taskArgs,
+		Visible:        true,
+		LastTask:       nil,
+		LastTaskResult: nil,
+		ID:             jobID,
+	}
+}
+
+func (j *SimpleDeleteUserDataJob) GetID() interfaces.JobID {
+	return j.ID
+}
+
+func (j *SimpleDeleteUserDataJob) GetTasks() []interfaces.Task {
+	return j.Tasks
+}
+
+func (j *SimpleDeleteUserDataJob) GetTaskArgs(taskID interfaces.TaskID) []*interfaces.TaskArg {
+	return j.TaskArgs[taskID]
+}
+
+func (j *SimpleDeleteUserDataJob) GetVisible() bool {
+	return j.Visible
+}
+
+func (j *SimpleDeleteUserDataJob) SetVisible(visible bool) {
+	j.Visible = visible
+}
+
+func (j *SimpleDeleteUserDataJob) GetLastTask() interfaces.Task {
+	return j.LastTask
+}
+
+func (j *SimpleDeleteUserDataJob) SetLastTask(task interfaces.Task) {
+	j.LastTask = task
+}
+
+func (j *SimpleDeleteUserDataJob) GetLastTaskPos() int {
+	return j.LastTaskPos
+}
+
+func (j *SimpleDeleteUserDataJob) SetLastTaskPos(pos int) {
+	j.LastTaskPos = pos
+}
+
+func (j *SimpleDeleteUserDataJob) GetLastTaskResult() *interfaces.TaskResult {
+	return j.LastTaskResult
+}
+
+func (j *SimpleDeleteUserDataJob) SetLastTaskResult(result *interfaces.TaskResult) {
+	j.LastTaskResult = result
+}
---
+++ examples/deleteuser/simple/job_test.go
@@ -0,0 +1,173 @@
+package simple
+
+import (
+	"fmt"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser/dummyservices"
+	"github.com/mtvarkovsky/goodjob/pkg/interfaces"
+	"github.com/mtvarkovsky/goodjob/pkg/processor"
+	"github.com/mtvarkovsky/goodjob/pkg/queue"
+	"github.com/mtvarkovsky/goodjob/pkg/storage"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestSimpleDeleteUser_Success(t *testing.T) {
+	userID := "user123"
+
+	var taskActionsLog []string
+
+	usersClient := dummyservices.NewUserServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user")
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	authClient := dummyservices.NewAuthServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user auth data")
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	ordersClient := dummyservices.NewOrdersServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user orders data")
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	job := NewSimpleDeleteUserDataJob(userID, usersClient, authClient, ordersClient)
+
+	q := queue.NewInMemQueue(2)
+
+	jobResultStorage := storage.NewInMemJobResultsStorage()
+	taskResultStorage := storage.NewInMemTaskResultStorage()
+
+	p := processor.NewV1Processor(q, jobResultStorage, taskResultStorage)
+
+	err := p.AddJob(job)
+	assert.NoError(t, err)
+
+	err = p.Start()
+	assert.NoError(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = p.Stop()
+	assert.NoError(t, err)
+
+	// check job result
+	jobRes, err := p.GetJobResult(job.GetID())
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		interfaces.JobResult{
+			ID:    job.GetID(),
+			Value: nil,
+			Err:   nil,
+		},
+		*jobRes,
+	)
+
+	// check queue logs
+	assert.Equal(
+		t,
+		[]string{
+			"successfully call safe delete user auth data",
+			"successfully call safe delete user",
+			"successfully call safe delete user orders data",
+		},
+		taskActionsLog,
+	)
+}
+
+func TestSimpleDeleteUser_Failure_CantDeleteOrders(t *testing.T) {
+	userID := "user123"
+
+	var taskActionsLog []string
+
+	usersClient := dummyservices.NewUserServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user")
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	authClient := dummyservices.NewAuthServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user auth data")
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	ordersClient := dummyservices.NewOrdersServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "failed call safe delete user orders data")
+			return fmt.Errorf("cant delete user orders")
+		},
+		func() error {
+			return nil
+		},
+	)
+
+	job := NewSimpleDeleteUserDataJob(userID, usersClient, authClient, ordersClient)
+
+	q := queue.NewInMemQueue(2)
+
+	jobResultStorage := storage.NewInMemJobResultsStorage()
+	taskResultStorage := storage.NewInMemTaskResultStorage()
+
+	p := processor.NewV1Processor(q, jobResultStorage, taskResultStorage)
+
+	err := p.AddJob(job)
+	assert.NoError(t, err)
+
+	err = p.Start()
+	assert.NoError(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = p.Stop()
+	assert.NoError(t, err)
+
+	// check job result
+	jobRes, err := p.GetJobResult(job.GetID())
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		interfaces.JobResult{
+			ID:    job.GetID(),
+			Value: nil,
+			Err:   fmt.Errorf("cant delete user orders"),
+		},
+		*jobRes,
+	)
+
+	// check queue logs
+	assert.Equal(
+		t,
+		[]string{
+			"successfully call safe delete user auth data",
+			"successfully call safe delete user",
+			"failed call safe delete user orders data",
+		},
+		taskActionsLog,
+	)
+}
