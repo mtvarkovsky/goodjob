5e7348567c7f7541633703eda8f1e73d5b9c7b4c
Mikhail Tvarkovsky <mtvarkovsky@gmail.com>
2023-03-20 02:17:30 +0400

    Add job for revertible delete user example

---
+++ examples/deleteuser/revertible/job.gen.go
@@ -0,0 +1,172 @@
+// This code is automatically generated. EDIT AT YOUR OWN RISK.
+
+package revertible
+
+import (
+	"fmt"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser/dummyservices"
+	"github.com/mtvarkovsky/goodjob/pkg/interfaces"
+	"github.com/oklog/ulid/v2"
+)
+
+type RevertibleDeleteUserDataJob struct {
+	LastTask       interfaces.Task
+	LastTaskPos    int
+	LastTaskResult *interfaces.TaskResult
+	ID             interfaces.JobID
+	JobArgs        []interfaces.JobArg
+	Tasks          []interfaces.Task
+	TaskArgs       map[interfaces.TaskID][]*interfaces.TaskArg
+	Visible        bool
+	RevertState    bool
+}
+
+func NewRevertibleDeleteUserDataJob(userID string, usersClient *dummyservices.UserServiceClient, authClient *dummyservices.AuthServiceClient, ordersClient *dummyservices.OrdersServiceClient) interfaces.RevertibleJob {
+	jobID := interfaces.JobID(fmt.Sprintf("delete user data with revert on failure (%s)", ulid.Make()))
+	jobArgs := []interfaces.JobArg{
+		{
+			Name:  "userID",
+			Value: userID,
+		},
+		{
+			Name:  "usersClient",
+			Value: usersClient,
+		},
+		{
+			Name:  "authClient",
+			Value: authClient,
+		},
+		{
+			Name:  "ordersClient",
+			Value: ordersClient,
+		},
+	}
+
+	taskIDs := map[string]interfaces.TaskID{
+		"safe delete user auth data":   interfaces.TaskID(fmt.Sprintf("safe delete user auth data (%s)", ulid.Make())),
+		"safe delete user data":        interfaces.TaskID(fmt.Sprintf("safe delete user data (%s)", ulid.Make())),
+		"safe delete user orders data": interfaces.TaskID(fmt.Sprintf("safe delete user orders data (%s)", ulid.Make())),
+	}
+	tasks := []interfaces.Task{
+		deleteuser.SafeDeleteAuthDataTask{
+			ID:    taskIDs["safe delete user auth data"],
+			JobID: jobID,
+		},
+		deleteuser.SafeDeleteUserTask{
+			ID:    taskIDs["safe delete user data"],
+			JobID: jobID,
+		},
+		deleteuser.SafeDeleteUserOrdersTask{
+			ID:    taskIDs["safe delete user orders data"],
+			JobID: jobID,
+		},
+	}
+	taskArgs := map[interfaces.TaskID][]*interfaces.TaskArg{
+		taskIDs["safe delete user auth data"]: {
+			{
+				Name:  "authClient",
+				Value: authClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+		taskIDs["safe delete user data"]: {
+			{
+				Name:  "usersClient",
+				Value: usersClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+		taskIDs["safe delete user orders data"]: {
+			{
+				Name:  "ordersClient",
+				Value: ordersClient,
+			},
+			{
+				Name:  "userID",
+				Value: userID,
+			},
+		},
+	}
+	return &RevertibleDeleteUserDataJob{
+		JobArgs:        jobArgs,
+		LastTaskPos:    0,
+		Tasks:          tasks,
+		TaskArgs:       taskArgs,
+		Visible:        true,
+		LastTask:       nil,
+		LastTaskResult: nil,
+		ID:             jobID,
+		RevertState:    false,
+	}
+}
+
+func (j *RevertibleDeleteUserDataJob) GetID() interfaces.JobID {
+	return j.ID
+}
+
+func (j *RevertibleDeleteUserDataJob) GetTasks() []interfaces.Task {
+	return j.Tasks
+}
+
+func (j *RevertibleDeleteUserDataJob) GetTaskArgs(taskID interfaces.TaskID) []*interfaces.TaskArg {
+	return j.TaskArgs[taskID]
+}
+
+func (j *RevertibleDeleteUserDataJob) GetVisible() bool {
+	return j.Visible
+}
+
+func (j *RevertibleDeleteUserDataJob) SetVisible(visible bool) {
+	j.Visible = visible
+}
+
+func (j *RevertibleDeleteUserDataJob) GetLastTask() interfaces.Task {
+	return j.LastTask
+}
+
+func (j *RevertibleDeleteUserDataJob) SetLastTask(task interfaces.Task) {
+	j.LastTask = task
+}
+
+func (j *RevertibleDeleteUserDataJob) GetLastTaskPos() int {
+	return j.LastTaskPos
+}
+
+func (j *RevertibleDeleteUserDataJob) SetLastTaskPos(pos int) {
+	j.LastTaskPos = pos
+}
+
+func (j *RevertibleDeleteUserDataJob) GetLastTaskResult() *interfaces.TaskResult {
+	return j.LastTaskResult
+}
+
+func (j *RevertibleDeleteUserDataJob) SetLastTaskResult(result *interfaces.TaskResult) {
+	j.LastTaskResult = result
+}
+
+func (j *RevertibleDeleteUserDataJob) GetTasksToRevert() []interfaces.RevertibleTask {
+	var tasksToRevert []interfaces.RevertibleTask
+	for i := j.LastTaskPos - 1; i >= 0; i-- {
+		task := j.Tasks[i]
+		switch t := task.(type) {
+		case interfaces.RevertibleTask:
+			tasksToRevert = append(tasksToRevert, t)
+		}
+	}
+	return tasksToRevert
+}
+
+func (j *RevertibleDeleteUserDataJob) GetRevertState() bool {
+	return j.RevertState
+}
+
+func (j *RevertibleDeleteUserDataJob) SetRevertState(revert bool) {
+	j.RevertState = revert
+}
---
+++ examples/deleteuser/revertible/job_test.go
@@ -0,0 +1,182 @@
+package revertible
+
+import (
+	"fmt"
+	"github.com/mtvarkovsky/goodjob/examples/deleteuser/dummyservices"
+	"github.com/mtvarkovsky/goodjob/pkg/interfaces"
+	"github.com/mtvarkovsky/goodjob/pkg/processor"
+	"github.com/mtvarkovsky/goodjob/pkg/queue"
+	"github.com/mtvarkovsky/goodjob/pkg/storage"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestRevertibleDeleteUser_Success(t *testing.T) {
+	userID := "user123"
+
+	var taskActionsLog []string
+
+	usersClient := dummyservices.NewUserServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user")
+			return nil
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call restore user")
+			return nil
+		},
+	)
+
+	authClient := dummyservices.NewAuthServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe user auth data")
+			return nil
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call restore user auth data")
+			return nil
+		},
+	)
+
+	ordersClient := dummyservices.NewOrdersServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "failed call safe delete user orders data")
+			return fmt.Errorf("cant delete user orders")
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call restore user orders data")
+			return nil
+		},
+	)
+
+	job := NewRevertibleDeleteUserDataJob(userID, usersClient, authClient, ordersClient)
+
+	q := queue.NewInMemQueue(2)
+
+	jobResultStorage := storage.NewInMemJobResultsStorage()
+	taskResultStorage := storage.NewInMemTaskResultStorage()
+
+	p := processor.NewV1Processor(q, jobResultStorage, taskResultStorage)
+
+	err := p.AddJob(job)
+	assert.NoError(t, err)
+
+	err = p.Start()
+	assert.NoError(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = p.Stop()
+	assert.NoError(t, err)
+
+	// check job result
+	jobRes, err := p.GetJobResult(job.GetID())
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		interfaces.JobResult{
+			ID:    job.GetID(),
+			Value: nil,
+			Err:   nil,
+		},
+		*jobRes,
+	)
+
+	// check queue logs
+	assert.Equal(
+		t,
+		[]string{
+			"successfully call safe user auth data",
+			"successfully call safe delete user",
+			"failed call safe delete user orders data",
+			"successfully call restore user",
+			"successfully call restore user auth data",
+		},
+		taskActionsLog,
+	)
+}
+
+func TestRevertibleDeleteUser_Failure_CantRestoreUser(t *testing.T) {
+	userID := "user123"
+
+	var taskActionsLog []string
+
+	usersClient := dummyservices.NewUserServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe delete user")
+			return nil
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "failed call restore user")
+			return fmt.Errorf("cant restore user")
+		},
+	)
+
+	authClient := dummyservices.NewAuthServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call safe user auth data")
+			return nil
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call restore user auth data")
+			return nil
+		},
+	)
+
+	ordersClient := dummyservices.NewOrdersServiceClient(
+		func() error {
+			taskActionsLog = append(taskActionsLog, "failed call safe delete user orders data")
+			return fmt.Errorf("cant delete user orders")
+		},
+		func() error {
+			taskActionsLog = append(taskActionsLog, "successfully call restore user orders data")
+			return nil
+		},
+	)
+
+	job := NewRevertibleDeleteUserDataJob(userID, usersClient, authClient, ordersClient)
+
+	q := queue.NewInMemQueue(2)
+
+	jobResultStorage := storage.NewInMemJobResultsStorage()
+	taskResultStorage := storage.NewInMemTaskResultStorage()
+
+	p := processor.NewV1Processor(q, jobResultStorage, taskResultStorage)
+
+	err := p.AddJob(job)
+	assert.NoError(t, err)
+
+	err = p.Start()
+	assert.NoError(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = p.Stop()
+	assert.NoError(t, err)
+
+	// check job result
+	jobRes, err := p.GetJobResult(job.GetID())
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		interfaces.JobResult{
+			ID:    job.GetID(),
+			Value: nil,
+			Err:   fmt.Errorf("cant restore user"),
+		},
+		*jobRes,
+	)
+
+	// check queue logs
+	assert.Equal(
+		t,
+		[]string{
+			"successfully call safe user auth data",
+			"successfully call safe delete user",
+			"failed call safe delete user orders data",
+			"failed call restore user",
+		},
+		taskActionsLog,
+	)
+}
